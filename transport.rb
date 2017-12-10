a17fee7a79c1229f45d65ab60ae9c7068c51bb00
Alex Edgcomb <alex.edgcomb@zybooks.com>
2017-12-10 09:56:55 -0800

    Fixed

--- transport.rb
+++ transport.rb
@@ -1,3 +1,4 @@
 b=gets.split.map{|n|n.chars}
 c=b.map{[]}
-gets.each_char{|d|b.map{|e|d>=e[0]&&d<=e[1]?c[b.index e].push(d):p}}puts c
+gets.each_char{|d|b.map{|e|d>=e[0]&&d<=e[1]?c[b.index e].push(d):p}}
+puts c
